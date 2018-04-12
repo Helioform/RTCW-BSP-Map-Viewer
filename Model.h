096722331654b2a1324e0a59ba7df39d411ffa79
Karl T <karl.tosin@gmail.com>
2018-04-12 07:42:18 -0400

    modified to not include a missing header file

--- Model.h
+++ Model.h
@@ -1,7 +1,4 @@
 #pragma once
-#include <assimp/Importer.hpp>      // C++ importer interface
-#include <assimp/scene.h>           // Output data structure
-#include <assimp/postprocess.h>     // Post processing flags
 
 class Model
 {
